7638fc2c84ffc8bca5c932c96c217dc73d1fb7c5
Welington <wellingtonfabi@gmail.com>
2025-07-01 21:23:30 -0300

    Desenvolvido API para aprndizado de Spring Security e Spring Data

---
+++ src/main/java/com/extremebike/aprendendospring/AprendendoSpringApplication.java
@@ -0,0 +1,18 @@
+package com.extremebike.aprendendospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.undo.UndoableEditSupport;
+
+@SpringBootApplication
+public class AprendendoSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AprendendoSpringApplication.class, args);
+
+
+	}
+
+}
---
+++ src/main/java/com/extremebike/aprendendospring/controller/dtos/UsusarioDTO.java
@@ -0,0 +1,17 @@
+package com.extremebike.aprendendospring.controller.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UsusarioDTO {
+
+    private String email;
+    private String senha;
+}
