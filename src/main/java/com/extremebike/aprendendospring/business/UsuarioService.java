0bcfa0f69e9127065a3d4bea6155763292a734a9
Welington <wellingtonfabi@gmail.com>
2025-07-01 22:42:25 -0300

    Alterações na usurarioSrvice e SecurityConfig

--- src/main/java/com/extremebike/aprendendospring/business/UsuarioService.java
+++ src/main/java/com/extremebike/aprendendospring/business/UsuarioService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UsuarioService {
 
-
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
